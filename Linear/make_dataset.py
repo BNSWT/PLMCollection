82d75f4a5d5c9086224b455928ac767f74d10101
Yuyang Zhou <zhouyuyang712@outlook.com>
2022-11-25 14:28:28 +0800

    Add logistic training for representations

--- Linear/make_dataset.py
+++ Linear/make_dataset.py
@@ -4,15 +4,54 @@ import os
 import re
 
 class FunctionSet(Dataset):
-    def __init__(self, all_file_path, a3m_dir):
-        self.a3m_dir = a3m_dir
-        sequences, labels = self.read_data(all_file_path)
-        self.repr, self.labels = self.make_repr(sequences, labels)
+    def __init__(self, all_file_path, a3m_dir, repr_path=None, label_path=None):
+        if repr_path is not None and label_path is not None:
+            self.repr = np.load(repr_path)
+            self.label = np.load(label_path)
+            return
+        if all_file_path == "data/Metal Ion Binding/test.txt":
+            self.setType = "test"
+            if a3m_dir == None:
+                repr_list = []
+                label_list = []
+                for path, dir_list, file_list in os.walk("data/Metal Ion Binding/"):
+                    for file_name in file_list:
+                        if "test-reprs-" in file_name:
+                            repr_list.append(np.load("data/Metal Ion Binding/"+file_name))
+                        elif "test-labels-" in file_name:
+                            label_list.append(np.load("data/Metal Ion Binding/"+file_name))
+                self.repr = np.append(repr_list[0],repr_list[1:],axis=0)
+                self.label = np.append(label_list[0],label_list[1:],axis=0)
+                print(self.repr.shape)
+                print(self.label.shape)
+            else:
+                self.a3m_dir = a3m_dir
+                sequences, labels = self.read_data(all_file_path)
+                self.repr, self.label = self.make_repr(sequences, labels)
+        else:
+            self.setType = "train"
+            if a3m_dir == None:
+                repr_list = []
+                label_list = []
+                for path, dir_list, file_list in os.walk("data/Metal Ion Binding/"):
+                    for file_name in file_list:
+                        if "reprs-" in file_name and "test" not in file_name:
+                            repr_list.append(np.load("data/Metal Ion Binding/"+file_name))
+                        elif "labels-" in file_name and "test" not in file_name:
+                            label_list.append(np.load("data/Metal Ion Binding/"+file_name))
+                self.repr = np.append(repr_list[0],repr_list[1:],axis=0)
+                self.label = np.append(label_list[0],label_list[1:],axis=0)
+                print(self.repr.shape)
+                print(self.label.shape)
+            else:
+                self.a3m_dir = a3m_dir
+                sequences, labels = self.read_data(all_file_path)
+                self.repr, self.label = self.make_repr(sequences, labels)
         return
     
     def __getitem__(self, i):
         index = i % self.__len__()
-        return self.repr[index], self.labels[index]
+        return self.repr[index], self.label[index]
     
     def __len__(self):
         return len(self.repr)
@@ -42,11 +81,16 @@ class FunctionSet(Dataset):
         g = os.walk(self.a3m_dir)
         for path, dir_list, file_list in g:
             for file_name in file_list:
+                if os.path.splitext(file_name)[-1] != ".a3m":
+                    continue
                 file = open(os.path.join(self.a3m_dir, file_name))
                 tags, seqs = self.parse_first_fasta(file.read())
                 if seqs in sequence or sequence in seqs:
                     name = tags
-                    repr_path = "data/Metal Ion Binding/repr/>" + name + "_msa_repr.npz.npy"
+                    if self.setType == "train":
+                        repr_path = "data/Metal Ion Binding/repr/>" + name + "_msa_repr.npz.npy"
+                    else:
+                        repr_path = "data/Metal Ion Binding/test-repr/>" + name + "_msa_repr.npz.npy"
                     if os.path.exists(repr_path):
                         return np.load(repr_path)
                     else:
@@ -62,24 +106,29 @@ class FunctionSet(Dataset):
             if repr is None:
                 del labels[index]
             else:
+                repr = self.align_repr(repr)
                 reprs.append(repr)
             index += 1
-            # if index == 10:
-            #     break
-            
-        reprs = self.align_reprs(reprs)
-        np.save("data/Metal Ion Binding/reprs.npz", np.array(reprs))
-        np.save("data/Metal Ion Binding/labels.npz", np.array(labels))
+            if index % 100 == 0:
+                print(f"Top {index} representations computed")
+            if index % 1000 == 0 or index == len(sequences):
+                part = index // 1000 - 1
+                if index == len(sequences):
+                    part += 1
+                if self.setType == "train":
+                    np.save(f"data/Metal Ion Binding/reprs-{part}.npz", np.array(reprs[part*1000:index]))
+                    np.save(f"data/Metal Ion Binding/labels-{part}.npz", np.array(labels[part*1000:index]))
+                else:
+                    np.save(f"data/Metal Ion Binding/test-reprs-{part}.npz", np.array(reprs[part*1000:index]))
+                    np.save(f"data/Metal Ion Binding/test-labels-{part}.npz", np.array(labels[part*1000:index]))
         return reprs, labels
     
-    def align_reprs(self, reprs):
-        new_reprs = []
-        for repr in reprs:
-            residue_num = repr.shape[1]
-            if residue_num > 256:
-                new_reprs.append(repr[:, 0:256, :])
-            elif residue_num == 256:
-                new_reprs.append(repr)
-            else:
-                new_reprs.append(np.pad(repr,((0, 0),(0, 256-residue_num),(0, 0))))
-        return new_reprs
+    def align_repr(self, repr):
+        residue_num = repr.shape[1]
+        if residue_num > 256:
+            new_repr=repr[:, 0:256, :]
+        elif residue_num == 256:
+            new_repr=repr
+        else:
+            new_repr=np.pad(repr,((0, 0),(0, 256-residue_num),(0, 0)))
+        return new_repr
--- Linear/train_logistic.py
+++ Linear/train_logistic.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
 import numpy as np
+import os
 from torch.utils.data.dataloader import DataLoader
 from make_dataset import FunctionSet
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 input_size = 512*256*256
 num_classes = 2
@@ -10,29 +13,62 @@ num_epochs = 100
 batch_size = 100
 learning_rate = 0.001
 
-train_data = FunctionSet("data/Metal Ion Binding/train.txt", "output/alignments")
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
+reprs_paths = ["data/Metal Ion Binding/reprs-0.npz.npy", "data/Metal Ion Binding/reprs-1.npz.npy", "data/Metal Ion Binding/reprs-2.npz.npy", "data/Metal Ion Binding/reprs-3.npz.npy", "data/Metal Ion Binding/reprs-4.npz.npy", "data/Metal Ion Binding/reprs-5.npz.npy"]
+labels_paths = ["data/Metal Ion Binding/labels-0.npz.npy", "data/Metal Ion Binding/labels-1.npz.npy", "data/Metal Ion Binding/labels-2.npz.npy", "data/Metal Ion Binding/labels-3.npz.npy", "data/Metal Ion Binding/labels-4.npz.npy", "data/Metal Ion Binding/labels-5.npz.npy"]
+
+
+# model = nn.Linear(input_size, num_classes)
+
+# criterion = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+
+# for epoch in range(num_epochs):
+#     for path_index in range(len(reprs_paths)):
+#         train_data = FunctionSet(None, None, repr_path=reprs_paths[path_index], label_path=labels_paths[path_index])
+#         train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)  
+#         total_step = len(train_loader)*len(reprs_paths)
+#         for i, (reprs, labels) in enumerate(train_loader):
+#             reprs = reprs.reshape(-1, input_size)
+            
+#             outputs = model(reprs)
+#             loss = criterion(outputs, labels)
+            
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+            
+#             _, predicted = torch.max(outputs.data, 1)
+#             total = labels.size(0)
+#             correct = (predicted == labels).sum()
+            
+#             print ('Epoch [{}/{}], Step [{}/{}], Accuracy: {:.4f},  Loss: {:.4f}' 
+#                     .format(epoch+1, num_epochs, i+1, total_step, correct/total, loss.item()))    
+
+# torch.save(model.state_dict(), 'linear_model.ckpt')
+
+      
 model = nn.Linear(input_size, num_classes)
+checkpoint = torch.load('linear_model.ckpt')
+model.load_state_dict(checkpoint)
+  
+test_reprs_paths = ["data/Metal Ion Binding/test-reprs-0.npz.npy", "data/Metal Ion Binding/test-reprs-1.npz.npy"] 
+test_labels_paths = ["data/Metal Ion Binding/test-labels-0.npz.npy", "data/Metal Ion Binding/test-labels-1.npz.npy"] 
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for path_index in range(len(test_reprs_paths)):
+        test_data = FunctionSet(None, None, repr_path=test_reprs_paths[path_index], label_path=test_labels_paths[path_index])
+        test_loader = DataLoader(test_data, batch_size=input_size, shuffle=False)
+        for reprs, labels in test_loader:
+            reprs = reprs.reshape(-1, input_size)
+            outputs = model(reprs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
 
-total_step = len(train_loader)
-for epoch in range(num_epochs):
-    for i, (reprs, labels) in enumerate(train_loader):
-        reprs = reprs.reshape(-1, input_size)
-        
-        outputs = model(reprs)
-        loss = criterion(outputs, labels)
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        _, predicted = torch.max(outputs.data, 1)
-        total = labels.size(0)
-        correct = (predicted == labels).sum()
-        
-        print ('Epoch [{}/{}], Step [{}/{}], Accuracy: {:.4f},  Loss: {:.4f}' 
-                .format(epoch+1, num_epochs, i+1, total_step, correct/total, loss.item()))    
+    print('Accuracy of the model: {} %'.format(100 * correct / total))
+
+torch.save(model.state_dict(), 'linear_model.ckpt')
